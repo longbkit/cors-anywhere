8a15c1739e1c32191ffddce94cc94e15321fb3ff
Long Luong <longbkit@gmail.com>
2024-10-13 23:38:22 +1000

    Update server.js to deploy to server-less env in vercel

--- server.js
+++ server.js
@@ -44,6 +44,8 @@ cors_proxy.createServer({
     // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
     xfwd: false,
   },
-}).listen(port, host, function() {
-  console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
+
+module.exports = (req, res) => {
+  server.emit('request', req, res);
+};
